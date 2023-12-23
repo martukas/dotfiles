916e7d5917d1f8678712871ee81851299e24c451
Martin Shetty <1972005+martukas@users.noreply.github.com>
2023-12-23 02:23:31 -0500

    reformat shell files

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,11 +7,11 @@ SUCCESS=0
 
 # Check if Linux
 case "${OSTYPE}" in
-linux*) ;;
-*)
-	echo "Error: This script only supports linux. You have: $OSTYPE."
-	exit $FAILURE
-	;;
+  linux*) ;;
+  *)
+    echo "Error: This script only supports linux. You have: $OSTYPE."
+    exit $FAILURE
+    ;;
 esac
 
 # Fail if any command fails
@@ -20,8 +20,8 @@ set -o pipefail
 
 # Make sure we are not in sudo
 if [ "${EUID}" -eq 0 ] && [ "$2" != "-f" ]; then
-	echo "Please do not run this script with root privileges!"
-	exit $FAILURE
+  echo "Please do not run this script with root privileges!"
+  exit $FAILURE
 fi
 
 echo "==============================================================="
@@ -39,8 +39,8 @@ sudo apt --yes install curl git-lfs ssh python3-pip python-is-python3 xclip
 
 browser_call='echo [RUNNING HEADLESS] Please open this URL manually: '
 if [ -n "$DISPLAY" ]; then
-	echo "GUI Enabled"
-	browser_call="python -m webbrowser"
+  echo "GUI Enabled"
+  browser_call="python -m webbrowser"
 fi
 
 wget 'https://github.com/martukas/dotfiles/raw/master/bootstrap/config_ssh.sh'
--- linux/profile2.sh
+++ linux/profile2.sh
@@ -3,62 +3,62 @@
 eval "$(thefuck --alias)"
 
 osinfo() {
-	uname -a
-	cat /etc/*-release
+  uname -a
+  cat /etc/*-release
 }
 
 sysinfo() {
-	# shellcheck disable=SC2024
-	sudo lshw -html >"${HOME}/Documents/system-info.html"
-	python -m webbrowser "${HOME}/Documents/system-info.html"
+  # shellcheck disable=SC2024
+  sudo lshw -html >"${HOME}/Documents/system-info.html"
+  python -m webbrowser "${HOME}/Documents/system-info.html"
 }
 
 reset-wifi() {
-	sudo /etc/init.d/network-manager stop
-	sudo rm /var/lib/NetworkManager/NetworkManager.state
-	sudo /etc/init.d/network-manager start
+  sudo /etc/init.d/network-manager stop
+  sudo rm /var/lib/NetworkManager/NetworkManager.state
+  sudo /etc/init.d/network-manager start
 }
 
 reset-bt() {
-	sudo service bluetooth restart
+  sudo service bluetooth restart
 }
 
 mouse-tweak() {
-	sudo systemctl enable --now logid
+  sudo systemctl enable --now logid
 }
 
 mouse-untweak() {
-	sudo systemctl disable --now logid
+  sudo systemctl disable --now logid
 }
 
 keychron-fn() {
-	echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
-	#  TODO: follow this to make persistent https://www.reddit.com/r/Keychron/comments/gptnpt/k1_no_access_to_fkeys_in_linux_help/
+  echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
+  #  TODO: follow this to make persistent https://www.reddit.com/r/Keychron/comments/gptnpt/k1_no_access_to_fkeys_in_linux_help/
 }
 
 create-user() {
-	echo "---------========= ACHTUNG =========---------"
-	echo "This will create a user on this system."
-	echo "With a home directory 'n shit..."
-	echo "... with SUDO powers!!!"
-	echo " "
-	echo " You better have their public ssh key ready!"
-	echo " "
-	read -rp "Yes-go or no-go? " answer
-	case ${answer:0:1} in
-	y | Y)
-		read -rp "Enter username: " user
-		sudo useradd -m -d "/home/${user}" -s /bin/bash "${user}"
-		sudo passwd "${user}"
-		sudo passwd -e "${user}"
-		sudo mkdir "/home/${user}/.ssh"
-		sudo touch "/home/${user}/.ssh/authorized_keys"
-		sudo chown -R "${user}:${user}" "/home/${user}/.ssh"
-		sudo chmod 700 "/home/${user}/.ssh"
-		sudo chmod 600 "/home/${user}/.ssh/authorized_keys"
-		sudo usermod -aG sudo "${user}"
-		sudo nano "/home/${user}/.ssh/authorized_keys"
-		;;
-	*) ;;
-	esac
+  echo "---------========= ACHTUNG =========---------"
+  echo "This will create a user on this system."
+  echo "With a home directory 'n shit..."
+  echo "... with SUDO powers!!!"
+  echo " "
+  echo " You better have their public ssh key ready!"
+  echo " "
+  read -rp "Yes-go or no-go? " answer
+  case ${answer:0:1} in
+    y | Y)
+      read -rp "Enter username: " user
+      sudo useradd -m -d "/home/${user}" -s /bin/bash "${user}"
+      sudo passwd "${user}"
+      sudo passwd -e "${user}"
+      sudo mkdir "/home/${user}/.ssh"
+      sudo touch "/home/${user}/.ssh/authorized_keys"
+      sudo chown -R "${user}:${user}" "/home/${user}/.ssh"
+      sudo chmod 700 "/home/${user}/.ssh"
+      sudo chmod 600 "/home/${user}/.ssh/authorized_keys"
+      sudo usermod -aG sudo "${user}"
+      sudo nano "/home/${user}/.ssh/authorized_keys"
+      ;;
+    *) ;;
+  esac
 }
