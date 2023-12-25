4cd4180dbfae33a24e3aee175e8e5bec19a61293
Martin Shetty <1972005+martukas@users.noreply.github.com>
2023-12-25 03:56:07 -0500

    different git bash prompt for WSL

--- common/bash/prompt.bash
+++ common/bash/prompt.bash
@@ -17,11 +17,14 @@ machine_name() {
 PROMPT_DIRTRIM=3
 
 # shellcheck disable=SC2143
-if [[ $(grep -i WSL /proc/version) ]]; then
-  echo "Bash is running on WSL"
+if [[ $(grep -i Microsoft /proc/version) ]]; then
+  GIT_PROMPT_THEME=Solarized_UserHost
   PS1="\n${COLOR_RED}#${COLOR_DEFAULT} ${COLOR_MAGENTA}\\u${COLOR_DEFAULT} ${COLOR_YELLOW}at${COLOR_DEFAULT} ${COLOR_CYAN}$(machine_name)${COLOR_DEFAULT} ${COLOR_YELLOW}in${COLOR_DEFAULT} ${COLOR_GREEN}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_BLUE}!${COLOR_DEFAULT} \"; fi)${COLOR_RED}>${COLOR_DEFAULT} "
   PS2="${COLOR_RED}>${COLOR_DEFAULT} "
 else
+  GIT_PROMPT_THEME=Minimal_UserHost
   PS1="\n${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
   PS2="${COLOR_BLUE}>${COLOR_DEFAULT} "
 fi
+
+export GIT_PROMPT_THEME
