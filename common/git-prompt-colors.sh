302f013d694b4e7d860959c4e899b627d1d96dfd
Martin Shetty <1972005+martukas@users.noreply.github.com>
2024-03-24 04:31:40 -0700

    fix colors

--- common/git-prompt-colors.sh
+++ common/git-prompt-colors.sh
@@ -1,10 +1,16 @@
 # shellcheck disable=all
 
 override_git_prompt_colors() {
+
+  function prompt_callback {
+    local PS1="$(gp_truncate_pwd)"
+    gp_set_window_title "$PS1"
+  }
+
   GIT_PROMPT_THEME_NAME="Custom"
   #  PathShort="\W"
-  GIT_PROMPT_START_USER="${BrightBlack}$(date +%H:%M:%S)${ResetColor} ${Yellow}${PathShort}${ResetColor}"
-  GIT_PROMPT_END="\n_LAST_COMMAND_INDICATOR_${BrightBlack}${CBC_PROMPT}${ResetColor} "
+  GIT_PROMPT_START_USER="${DimWhite}$(date +%H:%M:%S)${ResetColor} ${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_END="\n_LAST_COMMAND_INDICATOR_${DimWhite}${CBC_PROMPT}${ResetColor} "
   GIT_PROMPT_STAGED="${Red}● "      # the number of staged files/directories
   GIT_PROMPT_CLEAN="${BoldGreen}✔ " # a colored flag indicating a "clean" repo
   GIT_PROMPT_COMMAND_OK=""
