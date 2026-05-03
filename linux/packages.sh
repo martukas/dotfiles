e5107e4a88a60ea36c07bb79f09d1b0fcfec2204
Martin Shetty <1972005+martukas@users.noreply.github.com>
2026-05-02 21:03:50 -0700

    docker update

--- linux/packages.sh
+++ linux/packages.sh
@@ -172,7 +172,22 @@ function install_nordvpn() {
 }
 
 function install_docker() {
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+    | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update
+  if [ "$(getent group docker)" ]; then
+    echo "docker group already exists, skipping GID assignment"
+  else
+    sudo groupadd -g 1001 docker
+  fi
   sudo usermod -aG docker "${USER}"
+  sudo apt-get install -y docker-ce
 }
 
 function check_bing_wallpaper() {
